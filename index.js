02f71c312179d33fce502103ab6c02924b414b99
Israel <isradevelop@gmail.com>
2021-12-22 16:44:23 +0100

    prod

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const{dbConection} = require('./db/config')
@@ -27,6 +28,10 @@ app.use('/api/employee', require('./routes/employee.routes'));
 app.use('/api/schedule', require('./routes/schedule.routes'));
 app.use('/api/change', require('./routes/change.routes'));
 
+//manejar demas rutas(produccion)
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
